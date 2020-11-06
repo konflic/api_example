f7658be297a55fa4ef832464d42a3910e348c380
konflic <konflic@mail.ru>
2020-11-06 17:04:58 +0300

    Fix CORS

--- api/auth.py
+++ api/auth.py
@@ -51,25 +51,32 @@ def index():
 
 @auth_blueprint.route(routes.login, methods=["OPTIONS", "LOGIN"])
 def login():
-    data = request.get_json()
-
-    if data is None:
-        data = json.loads(request.get_data())
 
     if request.method == "OPTIONS":
         response = make_response({"status": "ok"}, 200)
         response.headers["Access-Control-Allow-Methods"] = "LOGIN, OPTIONS"
-    elif data is not None and data.get("login") == ADMIN["login"] and data.get("password") == ADMIN["password"]:
-        session['authorized'] = True
-        response = make_response({"status": "authorized"}, 200)
-        time.sleep(2)  # Imitating long response
     else:
-        # This is an example of wrong code given to auth error
-        # 402 is a Payment required status
-        response = make_response({
-            "error": "wrong credentials",
-            "credentials": str(data)
-        }, 402)
+
+        data = request.get_json()
+
+        if data is None:
+            try:
+                data = json.loads(request.get_data())
+            except json.decoder.JSONDecodeError:
+                data = {"login": "NONE", "password": "NONE"}
+
+        if data is not None and data.get("login") == ADMIN["login"] and data.get("password") == ADMIN["password"]:
+            session['authorized'] = True
+            response = make_response({"status": "authorized"}, 200)
+            time.sleep(2)  # Imitating long response
+        else:
+            # This is an example of wrong code given to auth error
+            # 402 is a Payment required status
+            response = make_response({
+                "error": "wrong credentials",
+                "credentials": str(data)
+            }, 402)
+
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Server"] = "WTF? 1.01 server"
     return response
